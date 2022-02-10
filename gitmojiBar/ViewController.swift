69f36adb6a479acc3ce682dce06a3abcd8dcbc0b
Fernando <techin.park@daum.net>
2022-02-10 17:03:42 +0900

    Initial Commit

---
+++ gitmojiBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gitmojiBar
+//
+//  Created by Fernando on 2022/02/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
